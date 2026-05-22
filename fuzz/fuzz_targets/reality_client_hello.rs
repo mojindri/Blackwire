9dccc55d96b4b728d14f7d7c234d4aab0f189237
mojnader <moji.ndr@gmail.com>
2026-05-22 18:36:14 +0200

    added fuzzy tests

---
+++ fuzz/fuzz_targets/reality_client_hello.rs
@@ -0,0 +1,12 @@
+#![no_main]
+
+#[path = "common.rs"]
+mod common;
+
+use libfuzzer_sys::fuzz_target;
+use proxy_transport::reality::parse_client_hello;
+
+fuzz_target!(|data: &[u8]| {
+    let data = common::bounded(data, 4096);
+    let _ = parse_client_hello(data);
+});
---
+++ fuzz/fuzz_targets/ss2022_chunk.rs
@@ -0,0 +1,14 @@
+#![no_main]
+
+#[path = "common.rs"]
+mod common;
+
+use libfuzzer_sys::fuzz_target;
+use proxy_protocol::ss2022::try_decrypt_chunk_for_fuzz;
+
+const SUBKEY: [u8; 32] = [0x33; 32];
+
+fuzz_target!(|data: &[u8]| {
+    let data = common::bounded(data, 4096);
+    let _ = try_decrypt_chunk_for_fuzz(&SUBKEY, data);
+});
