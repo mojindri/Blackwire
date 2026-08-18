bf415b23b3413d30725dc22672b9e9edd011b1e2
mojnader <moji.ndr@gmail.com>
2026-08-18 02:03:23 +0200

    Advance schema version in migration 0007

--- crates/blackwire-store/migrations/0007_reality_fallback_limits.sql
+++ crates/blackwire-store/migrations/0007_reality_fallback_limits.sql
@@ -5,3 +5,5 @@ ALTER TABLE reality_settings
     ADD COLUMN fallback_download_after_bytes BIGINT UNSIGNED NULL,
     ADD COLUMN fallback_download_bytes_per_sec BIGINT UNSIGNED NULL,
     ADD COLUMN fallback_download_burst_bytes_per_sec BIGINT UNSIGNED NULL;
+
+UPDATE blackwire_schema_version SET version = 7, updated_at = UTC_TIMESTAMP(6) WHERE singleton_id = 1;
